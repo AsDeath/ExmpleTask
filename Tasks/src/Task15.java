48a0fe99455c825421e57f0df6b4a7e9f5abe69a
Ashente <skaragachan@gmail.com>
2020-09-05 11:04:04 +0300

    t15

--- Tasks/src/Task15.java
+++ Tasks/src/Task15.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Task15 {
     public static void main(String[] args) {
-        int a = 5;
+        Scanner input  = new Scanner(System.in);
+        int a, b;
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.printf("Sum: %d\n",a+b);
+        System.out.printf("product: %d\n",a*b);
+        System.out.printf("Difference: %d\n",a-b);
+        System.out.printf("Division: %d\n",a/b);
+
     }
 }
