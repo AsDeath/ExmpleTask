75c5ea10fb0483ae52097a663fc5fcd743d3a11d
Ashente <skaragachan@gmail.com>
2020-09-06 16:33:19 +0300

    t16

---
+++ Tasks/src/Task16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task16 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter two numbers: ");
+        a = input.nextInt();
+        b = input.nextInt();
+        if(a == b) System.out.println("These numbers are equal");
+        else if(a>b) System.out.printf("Number %d is large",a);
+        else System.out.printf("Number %d is large",b);
+    }
+}
