b76d48b1d92a21fdcea3337ecde234e33808589d
Mortis <skaragachan@gmail.com>
2020-09-19 11:01:07 +0300

    chapter7task12

---
+++ Tasks/src/Chapter7Task12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Chapter7Task12 {
+    public static void main(String[] args){
+        final int N = 5;
+        int[] Arr = new int[N];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 5 numbers between 10 and 100: ");
+        for(int i=0; i<N; ){
+            System.out.printf("Enter number %d: ",i+1);
+            Arr[i] = input.nextInt();
+            if(Arr[i]>=10 && Arr[i]<=100) i++;
+            else System.out.println("Wrong data. Try again!");
+        }
+
+        System.out.println("Output unique data: ");
+        int count = 0;
+        for(int i=0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                if(i==j) continue;
+                if(Arr[i]==Arr[j]) count++;
+            }
+            if(count==0) System.out.printf("%3d   ", Arr[i]);
+            count=0;
+        }
+    }
+}
